10807e4863a48496a96e3cec600e4fd2a1b5de76
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-05-16 09:34:44 +0200

    added continue trainers up to job 30

---
+++ finetuning_experiment/compute_canada_script/continue_job20.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Echo time and hostname into log
+echo "Date:     $(date)"
+echo "Hostname: $(hostname)"
+
+# activate environment
+echo "Activating environment ..."
+source /home/p/plb/links/projects/aip-jcohen/plb/challenge/.venv_nnunet/bin/activate        # TODO: update to match the name of your environment
+
+# Define paths used:
+PATH_NNUNET_RAW_FOLDER="/home/p/plb/links/projects/aip-jcohen/plb/challenge/nnUNet_raw"
+PATH_OUTPUT="/home/p/plb/links/scratch/challenge"
+
+# Create the nnUNet_preprocessed and nnUNet_results folders
+mkdir -p $PATH_OUTPUT/nnUNet_preprocessed
+mkdir -p $PATH_OUTPUT/nnUNet_results
+
+# Export nnUNet paths
+export nnUNet_raw=${PATH_NNUNET_RAW_FOLDER}
+export nnUNet_preprocessed=${PATH_OUTPUT}/nnUNet_preprocessed
+export nnUNet_results=${PATH_OUTPUT}/nnUNet_results
+
+echo "nnUNet_raw: $nnUNet_raw"
+echo "nnUNet_preprocessed: $nnUNet_preprocessed"    
+echo "nnUNet_results: $nnUNet_results"
+
+# Define dataset values
+cuda_device=1
+dataset_number=120
+configurations="3d_fullres"
+fold=all
+planner="nnUNetPlannerResEncL"
+plans="nnUNetResEncUNetL_nonIso_Model1_Plans"
+trainer="nnUNetTrainerDAExt_DiceCELoss_noSmooth_500epochs"
+model_checkpoint="checkpoint_final.pth"
+pretrained_model_path="/home/p/plb/links/projects/aip-jcohen/plb/challenge/pretrained_multitalent/Dataset617_nativect/MultiTalent_trainer_4000ep__nnUNetResEncUNetL1x1x1_Plans_bs24__3d_fullres/fold_all/checkpoint_final.pth"
+
+
+# Model training:
+echo ""
+echo "Training the model"
+CUDA_VISIBLE_DEVICES=$cuda_device nnUNetv2_train  $dataset_number  $configurations $fold -p $plans -tr $trainer --c
---
+++ finetuning_experiment/compute_canada_script/continue_job5x6x11x12.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=aip-jcohen
+#SBATCH --job-name=challengejob5x6x11x12     # set a more descriptive job-name 
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=h100:4
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=300G
+#SBATCH --time=1-00:00:00   # DD-HH:MM:SS
+#SBATCH --output=/home/p/plb/links/scratch/challenge/%x_%A_v2.out
+#SBATCH --error=/home/p/plb/links/scratch/challenge/%x_%A_v2.err
+#SBATCH --mail-user=pierrelouis.benveniste03@gmail.com     # whenever the job starts/fails/completes, an email will be sent 
+#SBATCH --mail-type=ALL
+
+# Launch jobs
+parallel --verbose --jobs 4 ::: \
+  "(ts=\$(date '+%Y-%m-%d-%H-%M-%S'); bash /home/p/plb/links/projects/aip-jcohen/plb/challenge/ms-multi-spine-challenge-2024/finetuning_experiment/compute_canada_script/continue_job5.sh 2>&1 | tee /home/p/plb/links/scratch/challenge/logfile_job5_\$ts.txt)" \
+  "(ts=\$(date '+%Y-%m-%d-%H-%M-%S'); bash /home/p/plb/links/projects/aip-jcohen/plb/challenge/ms-multi-spine-challenge-2024/finetuning_experiment/compute_canada_script/continue_job6.sh 2>&1 | tee /home/p/plb/links/scratch/challenge/logfile_job6_\$ts.txt)" \
+  "(ts=\$(date '+%Y-%m-%d-%H-%M-%S'); bash /home/p/plb/links/projects/aip-jcohen/plb/challenge/ms-multi-spine-challenge-2024/finetuning_experiment/compute_canada_script/continue_job11.sh 2>&1 | tee /home/p/plb/links/scratch/challenge/logfile_job11_\$ts.txt)" \
+  "(ts=\$(date '+%Y-%m-%d-%H-%M-%S'); bash /home/p/plb/links/projects/aip-jcohen/plb/challenge/ms-multi-spine-challenge-2024/finetuning_experiment/compute_canada_script/continue_job12.sh 2>&1 | tee /home/p/plb/links/scratch/challenge/logfile_job12_\$ts.txt)"
