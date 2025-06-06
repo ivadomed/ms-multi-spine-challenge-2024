204dafd67528ff9091a656c38188da7b0c768741
Thomas Dagonneau <thdaga@romane>
2025-06-06 15:37:35 -0400

    the inference now runs with the 5 folds

--- docker-multimodal-model/scripts/run_inference.py
+++ docker-multimodal-model/scripts/run_inference.py
@@ -16,66 +16,32 @@ import os
 from pathlib import Path
 import torch
 
-# We define the environment variables here to avoid a warning from nnunetv2
-os.environ['nnUNet_raw'] = "../nnUNet_dataset"
-os.environ['nnUNet_preprocessed'] = "../nnUNet_preprocessed"
-os.environ['nnUNet_results']="../nnUNet_results"
+
 
 # Import for nnunetv2
-from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
 from batchgenerators.utilities.file_and_folder_operations import join
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Inference script")
     parser.add_argument("-i", "--input_image", type=str, required=True, help="Path to the input folder")
-    parser.add_argument("-m", "--model_path", type=str, required=True, help="Path to the model folder")
+    #parser.add_argument("-m", "--model_path", type=str, required=True, help="Path to the model folder")
     parser.add_argument("-o", "--output_folder", type=str, required=True, help="Path to the output folder")
     return parser.parse_args()
 
 
-def run_inference(input_image, model_path, output_folder):
+def run_inference(input_image, output_folder):
 
     # Build a temp folder in the output folder
     temp_folder = os.path.join(output_folder, "temp_inference")
     os.makedirs(temp_folder, exist_ok=True)
     
-    # Initialize the model 
-    predictor = nnUNetPredictor(
-        tile_step_size=0.5,     # changing it from 0.5 to 0.9 makes inference faster
-        use_gaussian=True,                      # applies gaussian noise and gaussian blur
-        use_mirroring=True,                    # test time augmentation by mirroring on all axes
-        device=torch.device('cpu'),
-        verbose=False,
-        verbose_preprocessing=False,
-        allow_tqdm=True
-    )
-        
-    # initializes the network architecture, loads the checkpoint
-    predictor.initialize_from_trained_model_folder(
-        model_path,
-        use_folds=[0],  # if fold_all is set, we use all folds, else we use fold 0
-        checkpoint_name='checkpoint_best.pth',
-    )
-
-    # Run inference on the input image
-    predictor.predict_from_files(
-        list_of_lists_or_source_folder=[[input_image]],
-        output_folder_or_list_of_truncated_output_files=temp_folder,
-        save_probabilities=True,
-        overwrite=True,
-        num_processes_preprocessing=8,
-        num_processes_segmentation_export=8,
-        folder_with_segs_from_prev_stage=None,
-        num_parts=1,
-        part_id=0
-    )
+    for fold in range(5):
+        # Run inference for the fold
+        assert os.system(f"nnUNetv2_predict -i {input_image} -o {output_folder}/fold_{fold}/prediction -d 200 -p nnUNetResEncUNetLPlans -tr nnUNetTrainerDA5_150epochs -c 2d -f {fold} -chk checkpoint_best_{fold}.pth ") == 0
     
-    # Build output image path (output folder and image name)
-    output_image = os.path.join(temp_folder, Path(input_image).name)
-
-    return output_image
+    return output_folder
 
 
 if __name__ == "__main__":
     args = parse_args()
-    output_image = run_inference(args.input_image, args.model_path, args.output_folder)
+    output_image = run_inference(args.input_image, args.output_folder)
