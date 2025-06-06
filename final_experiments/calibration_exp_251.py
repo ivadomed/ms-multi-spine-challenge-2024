bb7bafcfdb087de68180b00d7e27a8088fc83c31
Pierre-Louis Benveniste <pierrelouis.benveniste03@gmail.com>
2025-06-06 13:37:07 -0400

    added scripts for final experiments

---
+++ final_experiments/calibration_exp_251.py
@@ -0,0 +1,49 @@
+"""
+This script performs calibration of the threshold during mask binarization.
+"""
+import json
+import os
+
+def main():
+
+    image_dict = "/home/plbenveniste/net/challenge-multi-spine/final_compute_canada_results/images_dict.json"
+
+    output_folder = "/home/plbenveniste/net/challenge-multi-spine/final_compute_canada_results/exp_251_prep"
+
+    # load the json file
+    with open(image_dict, "r") as f:
+        images_dict = json.load(f)
+
+    training_images = images_dict["training"]
+    testing_images = images_dict["testing"]
+    images = {**training_images, **testing_images}
+
+    # iterate over the images
+    for image in images:
+        # If contrast is T2, we skip
+        if images[image]["contrast"] == "T2w":
+            continue
+        # Build a folder for each subject
+        sub_folder = os.path.join(output_folder, images[image]["subject_name"])
+        print("Subject name", images[image]["subject_name"])
+
+        # Build path to the lesion masks
+        mask = os.path.join(sub_folder,"predictions", "merged_segmentation_masked.nii.gz")
+
+        # Build an output folder
+        output_folder_calibration = os.path.join(sub_folder, "calibration")
+        os.makedirs(output_folder_calibration, exist_ok=True)
+
+
+        for thresh in [0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]:
+            # We build the output path
+            output_path = os.path.join(output_folder_calibration, f"merged_segmentation_masked_thresh_{thresh}.nii.gz")
+            # We perform the binarization
+            assert os.system(f"sct_maths -i {mask} -bin {thresh} -o {output_path} ") == 0
+
+        break
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ final_experiments/merge_pred_exp_251.py
@@ -0,0 +1,43 @@
+"""
+This script merges both prediction by doing a simple average of both predictions.
+"""
+import json
+import os
+
+
+def main():
+
+    image_dict = "/home/plbenveniste/net/challenge-multi-spine/final_compute_canada_results/images_dict.json"
+
+    output_folder = "/home/plbenveniste/net/challenge-multi-spine/final_compute_canada_results/exp_251_prep"
+
+    # load the json file
+    with open(image_dict, "r") as f:
+        images_dict = json.load(f)
+
+    training_images = images_dict["training"]
+    testing_images = images_dict["testing"]
+    images = {**training_images, **testing_images}
+
+    # iterate over the images
+    for image in images:
+        # If contrast is T2, we skip
+        if images[image]["contrast"] == "T2w":
+            continue
+        # Build a folder for each subject
+        sub_folder = os.path.join(output_folder, images[image]["subject_name"])
+        print("Subject name", images[image]["subject_name"])
+
+        # Build path to the lesion masks
+        lesion_mask_t2 = os.path.join(sub_folder,"predictions", "t2w_segmentation_masked.nii.gz")
+        lesion_mask_psir = os.path.join(sub_folder,"predictions", "psir_segmentation_masked.nii.gz")
+
+        # Add both segmentation masks
+        assert os.system(f"sct_maths -i {lesion_mask_t2} -add {lesion_mask_psir} -o {os.path.join(sub_folder,'predictions', 'merged_segmentation_masked.nii.gz')}") == 0
+        ## Divide by 2 to average the predictions
+        assert os.system(f"sct_maths -i {os.path.join(sub_folder,'predictions', 'merged_segmentation_masked.nii.gz')} -div 2 -o {os.path.join(sub_folder,'predictions', 'merged_segmentation_masked.nii.gz')}") == 0
+        break
+
+
+if __name__ == "__main__":
+    main()
