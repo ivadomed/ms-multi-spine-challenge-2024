4130dcafa655f33be4364f183fce5d219610b3c6
Thomas Dagonneau <thdaga@romane>
2025-06-13 13:58:58 -0400

    added the postprocessing commands to main

--- docker-multimodal-model/scripts/main.py
+++ docker-multimodal-model/scripts/main.py
@@ -23,7 +23,12 @@ import os
 from listing_inputs import listing_input_files
 from image_preprocessing import preprocess_images
 from run_inference import run_inference
-from pred_postprocessing import postprocess_segmentation
+from remove_lesions_outside_sc import remove_lesions_outside_sc
+from remove_lesions_max_value import remove_lesions_max_value
+from merge_predictions import merge_predictions
+from binarize_prediction import binarize_prediction
+from remove_small_lesions import remove_small_lesions
+from format_segmentation import format_segmentation
 
 
 def parse_args():
@@ -49,25 +54,50 @@ def main():
 
     # 1. List all the files in the input folder 
     list_files = listing_input_files(input_folder)
-    print(list_files)
 
     # 2. Now we perform image preprocessing
-    subj_dict, preprocessed_images, t2w_raw_image, t = preprocess_images(list_files, temp_folder)
+    subj_dict = preprocess_images(list_files, temp_folder)
 
-    # Build the path to the model: it is stored in the repo in a folder called "trained-model"
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    parent_dir = os.path.dirname(current_dir)
+    
 
     # 3. Now we perform inference on the preprocessed images
     
     print(f"Running inference...")
-    predicted_segmentations = run_inference(preprocessed_images, temp_folder, t)
+    subj_dict = run_inference(subj_dict, temp_folder)
 
-    # 4. Now we perform postprocessing of each predicted segmentation
+    ### POST-PROCESSING ### 
     
-    postprocessed_segmentations = (postprocess_segmentation(predicted_segmentations, subj_dict, t2w_raw_image))
+    # 4. Now we remove lesions outside of the spinal cord
+    subj_dict = remove_lesions_outside_sc(subj_dict, temp_folder)
 
-    # 5. Fusion of information
+    # subj_dict = {'t2_raw': '../sub-001/rawdata/sub-001/11-001_T2.nii.gz', 't2_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_T2.nii.gz', 't2_inference_file': '../output_sub-001/temp/t2w_inference.nii.gz', 'other_images': [{'image_raw': '../sub-001/rawdata/sub-001/11-001_STIR.nii.gz', 'image_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_STIR.nii.gz', 'contrast': 'STIR', 'inference_file': '../output_sub-001/temp/STIR_inference.nii.gz', 'segmentation_file': '../output_sub-001/temp/STIR_segmentation.nii.gz', 'segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/STIR_segmentation_rmv_lesions_outside_sc.nii.gz'}], 't2_segmentation_file': '../output_sub-001/temp/t2w_segmentation.nii.gz', 't2_segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/t2w_segmentation_rmv_lesions_outside_sc.nii.gz'}
+
+    # 5. Now we remove lesions where max voxel value is below 0.8
+    subj_dict = remove_lesions_max_value(subj_dict, temp_folder)
+
+    # subj_dict = {'t2_raw': '../sub-001/rawdata/sub-001/11-001_T2.nii.gz', 't2_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_T2.nii.gz', 't2_inference_file': '../output_sub-001/temp/t2w_inference.nii.gz', 'other_images': [{'image_raw': '../sub-001/rawdata/sub-001/11-001_STIR.nii.gz', 'image_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_STIR.nii.gz', 'contrast': 'STIR', 'inference_file': '../output_sub-001/temp/STIR_inference.nii.gz', 'segmentation_file': '../output_sub-001/temp/STIR_segmentation.nii.gz', 'segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/STIR_segmentation_rmv_lesions_outside_sc.nii.gz', 'segmentation_file_rmv_lesions_max_value': '../output_sub-001/temp/STIR_segmentation_file_rmv_lesions_max_value.nii.gz'}], 't2_segmentation_file': '../output_sub-001/temp/t2w_segmentation.nii.gz', 't2_segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/t2w_segmentation_rmv_lesions_outside_sc.nii.gz', 't2_segmentation_file_rmv_lesions_max_value': '../output_sub-001/temp/t2w_segmentation_file_rmv_lesions_max_value.nii.gz'}
+
+    # 6. Merge predictions across contrasts
+    subj_dict = merge_predictions(subj_dict, temp_folder)
+
+    # 7. Binarization of the merged predictions (threshold = 0.8)
+    subj_dict = binarize_prediction(subj_dict, temp_folder)
+
+    # 8. Remove small lesions  (less than 18 voxels)
+    subj_dict = remove_small_lesions(subj_dict, temp_folder)
+
+    subj_dict = {'t2_raw': '../sub-001/rawdata/sub-001/11-001_T2.nii.gz', 't2_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_T2.nii.gz', 't2_inference_file': '../output_sub-001/temp/t2w_inference.nii.gz', 'other_images': [{'image_raw': '../sub-001/rawdata/sub-001/11-001_STIR.nii.gz', 'image_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_STIR.nii.gz', 'contrast': 'STIR', 'inference_file': '../output_sub-001/temp/STIR_inference.nii.gz', 'segmentation_file': '../output_sub-001/temp/STIR_segmentation.nii.gz', 'segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/STIR_segmentation_rmv_lesions_outside_sc.nii.gz', 'segmentation_file_rmv_lesions_max_value': '../output_sub-001/temp/STIR_segmentation_file_rmv_lesions_max_value.nii.gz'}], 't2_segmentation_file': '../output_sub-001/temp/t2w_segmentation.nii.gz', 't2_segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/t2w_segmentation_rmv_lesions_outside_sc.nii.gz', 't2_segmentation_file_rmv_lesions_max_value': '../output_sub-001/temp/t2w_segmentation_file_rmv_lesions_max_value.nii.gz', 'merged_lesion_mask': '../output_sub-001/temp/merged_lesion_mask.nii.gz', 'binarized_lesion_mask': '../output_sub-001/temp/binarized_lesion_mask.nii.gz', 'lesion_mask_rmv_small_lesion': '../output_sub-001/temp/lesion_mask_rmv_small_lesion.nii.gz'}
+
+
+    ############################
+    #### FORMAT PREDICTIONS ####
+    ############################
+    # 9. Format the prediction
+    subj_dict = format_segmentation(subj_dict, output_folder)
+
+    # Remove the temporary folder
+    assert os.system(f"rm -rf {temp_folder}") == 0, "Failed to remove the temporary folder."
+
      
 
 
