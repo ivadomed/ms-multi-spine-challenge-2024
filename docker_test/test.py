4195941741e766565cafec2b3323dc7a847ce877
Thomas Dagonneau <thdaga@romane>
2025-06-16 10:50:49 -0400

    new_structure

--- docker_test/test.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-print("CUDA available:", torch.cuda.is_available())
-print("Number of GPUs:", torch.cuda.device_count())
-if torch.cuda.is_available():
-    print("GPU Name:", torch.cuda.get_device_name(0))
