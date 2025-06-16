0862ec4f7912249c84cb00274e46178c29df9554
Thomas Dagonneau <thdaga@romane>
2025-06-16 10:33:04 -0400

    final version of main.py

--- docker-multimodal-model/scripts/main.py
+++ docker-multimodal-model/scripts/main.py
@@ -5,9 +5,8 @@ The steps are the following:
 2. Image preprocessing
 3. Inference on images
 4. Postprocessing of the predicted segmentations
-6. Fusion of information
-7. Instance segmentation
-8. Saving of instance segmentation and csv file with instance probabilities
+5. Instance segmentation
+6. Saving of instance segmentation and csv file with instance probabilities
 
 Args:
     --input_folder: path to the input folder containing the images of only one subject
@@ -16,7 +15,7 @@ Args:
 Returns: 
     None
 
-Author: Pierre-Louis Benveniste
+Author: Thomas Dagonneau & Pierre-Louis Benveniste
 """
 import argparse
 import os
@@ -51,44 +50,41 @@ def main():
     temp_folder = os.path.join(output_folder, "temp")
     os.makedirs(temp_folder, exist_ok=True)
 
+    #######################
+    #### PREPROCESSING ####
+    #######################
+
     # 1. List all the files in the input folder 
     list_files = listing_input_files(input_folder)
 
     # 2. Now we perform image preprocessing
     subj_dict = preprocess_images(list_files, temp_folder)
-    print(subj_dict)
-
+   
+    ###################
+    #### INFERENCE ####
+    ###################
     
-
     # 3. Now we perform inference on the preprocessed images
     
     print(f"Running inference...")
     subj_dict = run_inference(subj_dict, temp_folder)
-    print(subj_dict)
 
-    ### POST-PROCESSING ### 
+    #########################
+    #### POST-PROCESSING ####
+    #########################
 
     # 4. Postprocess the segmentations
     subj_dict = postprocess_segmentation(subj_dict)
     print(subj_dict)
 
-    
-    # 4. Now we remove lesions outside of the spinal cord
+    #  Now we remove lesions outside of the spinal cord
     subj_dict = remove_lesions_outside_sc(subj_dict, temp_folder)
-    #print(subj_dict)
 
-
-    # subj_dict = {'t2_raw': '../sub-001/rawdata/sub-001/11-001_T2.nii.gz', 't2_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_T2.nii.gz', 't2_inference_file': '../output_sub-001/temp/t2w_inference.nii.gz', 'other_images': [{'image_raw': '../sub-001/rawdata/sub-001/11-001_STIR.nii.gz', 'image_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_STIR.nii.gz', 'contrast': 'STIR', 'inference_file': '../output_sub-001/temp/STIR_inference.nii.gz', 'segmentation_file': '../output_sub-001/temp/STIR_segmentation.nii.gz', 'segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/STIR_segmentation_rmv_lesions_outside_sc.nii.gz'}], 't2_segmentation_file': '../output_sub-001/temp/t2w_segmentation.nii.gz', 't2_segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/t2w_segmentation_rmv_lesions_outside_sc.nii.gz'}
-
-    # 5. Now we remove lesions where max voxel value is below 0.8
+    #  Now we remove lesions where max voxel value is below 0.3
     subj_dict = remove_lesions_max_value(subj_dict, temp_folder)
     
- 
-    # 8. Remove small lesions  (less than 18 voxels)
+    # Remove small lesions  (less than 18 voxels)
     subj_dict = remove_small_lesions(subj_dict, temp_folder)
-
-    #subj_dict = {'t2_raw': '../sub-001/rawdata/sub-001/11-001_T2.nii.gz', 't2_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_T2.nii.gz', 't2_inference_file': '../output_sub-001/temp/t2w_inference.nii.gz', 'other_images': [{'image_raw': '../sub-001/rawdata/sub-001/11-001_STIR.nii.gz', 'image_preproc': '../sub-001/derivatives/preprocessed/sub-001/11-001_STIR.nii.gz', 'contrast': 'STIR', 'inference_file': '../output_sub-001/temp/STIR_inference.nii.gz', 'segmentation_file': '../output_sub-001/temp/STIR_segmentation.nii.gz', 'segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/STIR_segmentation_rmv_lesions_outside_sc.nii.gz', 'segmentation_file_rmv_lesions_max_value': '../output_sub-001/temp/STIR_segmentation_file_rmv_lesions_max_value.nii.gz'}], 't2_segmentation_file': '../output_sub-001/temp/t2w_segmentation.nii.gz', 't2_segmentation_file_rmv_lesions_outside_sc': '../output_sub-001/temp/t2w_segmentation_rmv_lesions_outside_sc.nii.gz', 't2_segmentation_file_rmv_lesions_max_value': '../output_sub-001/temp/t2w_segmentation_file_rmv_lesions_max_value.nii.gz', 'merged_lesion_mask': '../output_sub-001/temp/merged_lesion_mask.nii.gz', 'binarized_lesion_mask': '../output_sub-001/temp/binarized_lesion_mask.nii.gz', 'lesion_mask_rmv_small_lesion': '../output_sub-001/temp/lesion_mask_rmv_small_lesion.nii.gz'}
-
 
     ############################
     #### FORMAT PREDICTIONS ####
@@ -98,9 +94,6 @@ def main():
 
     # Remove the temporary folder
     assert os.system(f"rm -rf {temp_folder}") == 0, "Failed to remove the temporary folder."
-
-     
-
 
     return None
 
